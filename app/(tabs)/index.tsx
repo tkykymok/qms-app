b9a626426d4e1b8347857bd9cf82694670ca8fb9
tkykymok <tkykymok2@icloud.com>
2024-01-17 18:32:18 +0900

    GoogleMapコンポーネントを修正

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,74 @@
 import { Text, View } from "react-native";
 import BottomSheet, { BottomSheetBackdrop } from "@gorhom/bottom-sheet";
-import { useRef } from "react";
-import GoogleMapWithInputView from "../../components/organisms/GoogleMapWithInputView";
+import React, { useEffect, useRef, useState } from "react";
+import { Marker, Region } from "react-native-maps";
+import { GooglePlaceDetail } from "react-native-google-places-autocomplete";
+import GoogleMapView from "../../components/molecules/GoogleMapView";
+import GoogleMapSearchInput from "../../components/molecules/GoogleMapSearchInput";
+import * as Location from "expo-location";
 
 const Index = () => {
   // ref
   const bottomSheetRef = useRef<BottomSheet>(null);
 
+  // 現在地
+  const [currentPosition, setCurrentPosition] = useState<Region>({
+    latitude: 35.685175,
+    longitude: 139.7502246,
+    latitudeDelta: 0.03,
+    longitudeDelta: 0.03,
+  });
+
+  useEffect(() => {
+    // 現在地の取得
+    getCurrentLocation().then((location) => {
+      setCurrentPosition({
+        latitude: location!.coords.latitude,
+        longitude: location!.coords.longitude,
+        latitudeDelta: 0.03,
+        longitudeDelta: 0.03,
+      });
+    });
+  }, []);
+
+  const handleOnPressSearchResult = (details: GooglePlaceDetail | null) => {
+    // 検索結果をタップした時の処理
+    if (details) {
+      const newRegion = {
+        latitude: details.geometry.location.lat,
+        longitude: details.geometry.location.lng,
+        latitudeDelta: 0.027,
+        longitudeDelta: 0.027,
+      };
+      setCurrentPosition(newRegion);
+    }
+  };
+
+  const locations: {
+    id: number;
+    latitude: number;
+    longitude: number;
+  }[] = [{ id: 1, latitude: 35.685175, longitude: 139.7502246 }];
+
   return (
     <>
       {/* GoogleMap */}
-      <GoogleMapWithInputView />
+      <GoogleMapView currentPosition={currentPosition}>
+        {locations &&
+          locations.map((location) => (
+            <Marker
+              key={location.id}
+              coordinate={{
+                latitude: location.latitude,
+                longitude: location.longitude,
+              }}
+              onPress={() => bottomSheetRef.current?.expand()}
+            />
+          ))}
+      </GoogleMapView>
+      <GoogleMapSearchInput
+        handleOnPressSearchResult={handleOnPressSearchResult}
+      />
 
       {/* BottomSheet 店舗詳細 */}
       <BottomSheet
@@ -31,3 +89,16 @@ const Index = () => {
 };
 
 export default Index;
+
+/**
+ * 現在地の取得
+ */
+async function getCurrentLocation() {
+  let { status } = await Location.requestForegroundPermissionsAsync();
+  if (status !== "granted") {
+    // 許可が得られなかった場合の処理
+    return;
+  }
+
+  return await Location.getCurrentPositionAsync({});
+}
--- components/molecules/GoogleMapView.tsx
+++ components/molecules/GoogleMapView.tsx
@@ -1,16 +1,21 @@
 import MapView, { PROVIDER_GOOGLE, Region } from "react-native-maps";
-import { FC } from "react";
+import { FC, ReactNode } from "react";
 
 interface GoogleMapViewProps {
+  children?: ReactNode;
   currentPosition: Region;
 }
 
 /**
  * GoogleMapView
+ * @param children
  * @param currentPosition
  * @constructor
  */
-const GoogleMapView: FC<GoogleMapViewProps> = ({ currentPosition }) => (
+const GoogleMapView: FC<GoogleMapViewProps> = ({
+  children,
+  currentPosition,
+}) => (
   <MapView
     mapPadding={{ top: 15, right: 0, bottom: 0, left: 0 }}
     provider={PROVIDER_GOOGLE}
@@ -18,7 +23,9 @@ const GoogleMapView: FC<GoogleMapViewProps> = ({ currentPosition }) => (
     region={currentPosition}
     showsUserLocation
     showsMyLocationButton
-  />
+  >
+    {children}
+  </MapView>
 );
 
 export default GoogleMapView;
